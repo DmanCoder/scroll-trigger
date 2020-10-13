a82f0e589b7998d6be02c4bc84dde3a1d38297d0
DmanCoder <ktdenis.1@live.com>
2020-10-13 13:05:40 +1000

    Added basic fade

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import image1 from './img/img_greensock-logo.png';
 import image2 from './img/img_project01-icon.svg';
 import image3 from './img/img_project02-icon.svg';
 import image4 from './img/img_project03-icon.svg';
 
+import './App.css';
+
 function App() {
+  useEffect(() => {
+    gsap.to('#intro img', { duration: 1, opacity: 0 });
+  }, []);
+
   return (
     <div className="App">
       <div id="intro">
-        <div class="content">
+        <div className="content">
           <img src={image1} alt="AYYY" />
           <h1>The Basics</h1>
           <p>
@@ -20,19 +27,19 @@ function App() {
         </div>
       </div>
 
-      <div id="main" class="main-container">
-        <div id="project01" class="project">
+      <div id="main" className="main-container">
+        <div id="project01" className="project">
           <img src={image2} alt="AYYY" />
           <h2>Project Title</h2>
-          <p class="info">
+          <p className="info">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
             dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
           </p>
         </div>
 
-        <div class="bcg-parallax">
-          <div class="bcg"></div>
-          <div class="content-wrapper">
+        <div className="bcg-parallax">
+          <div className="bcg"></div>
+          <div className="content-wrapper">
             <h1>Section With Parallax Effect</h1>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
@@ -42,27 +49,27 @@ function App() {
           </div>
         </div>
 
-        <div id="project02" class="project">
+        <div id="project02" className="project">
           <img src={image3} alt="AYYY" />
           <h2>Project Title</h2>
-          <p class="info">
+          <p className="info">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
             dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
           </p>
         </div>
 
-        <div id="project03" class="project">
+        <div id="project03" className="project">
           <img src={image4} alt="AYYY" />
           <h2>Project Title</h2>
-          <p class="info">
+          <p className="info">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
             dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
           </p>
         </div>
       </div>
 
-      <div class="footer-container">
-        <footer class="wrapper">
+      <div className="footer-container">
+        <footer className="wrapper">
           <h3>
             2020 &copy;
             <a href="https://ihatetomatoes.net" target="_blank">
