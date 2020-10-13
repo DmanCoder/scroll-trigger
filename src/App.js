273c52ea88bfe83d4ad2f3cb18f7671d2903f5d6
DmanCoder <ktdenis.1@live.com>
2020-10-13 12:56:17 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import image1 from './img/img_greensock-logo.png';
+import image2 from './img/img_project01-icon.svg';
+import image3 from './img/img_project02-icon.svg';
+import image4 from './img/img_project03-icon.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="intro">
+        <div class="content">
+          <img src={image1} alt="AYYY" />
+          <h1>The Basics</h1>
+          <p>
+            Short and sharp ScrollTrigger demos, teaching you the basics of
+            GreenSock's ScrollTrigger API.
+          </p>
+        </div>
+      </div>
+
+      <div id="main" class="main-container">
+        <div id="project01" class="project">
+          <img src={image2} alt="AYYY" />
+          <h2>Project Title</h2>
+          <p class="info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+            dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
+          </p>
+        </div>
+
+        <div class="bcg-parallax">
+          <div class="bcg"></div>
+          <div class="content-wrapper">
+            <h1>Section With Parallax Effect</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+              dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.
+              Sed.
+            </p>
+          </div>
+        </div>
+
+        <div id="project02" class="project">
+          <img src={image3} alt="AYYY" />
+          <h2>Project Title</h2>
+          <p class="info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+            dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
+          </p>
+        </div>
+
+        <div id="project03" class="project">
+          <img src={image4} alt="AYYY" />
+          <h2>Project Title</h2>
+          <p class="info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in
+            dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed.
+          </p>
+        </div>
+      </div>
+
+      <div class="footer-container">
+        <footer class="wrapper">
+          <h3>
+            2020 &copy;
+            <a href="https://ihatetomatoes.net" target="_blank">
+              Ihatetomatoes.net
+            </a>
+            |
+            <a href="https://twitter.com/ihatetomatoes" target="_blank">
+              @ihatetomatoes
+            </a>
+            | Animate responsibly!
+          </h3>
+        </footer>
+      </div>
     </div>
   );
 }
